5909eee61adf8e0589ceb9deecb1d2e2c96e6744
Vivek7452 <info4vivekbhardwaj@gmail.com>
2022-04-04 22:33:50 +0530

    all done

---
+++ config/mongoose_configuration.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');//Importing The moongoose library
+
+// const db= 'mongodb://localhost/hotel_db';
+
+mongoose.connect('mongodb://localhost:27017/hotel');//Create connection with database
+
+const data_base=mongoose.connection;
+
+data_base.on('error',console.error.bind(console,'DataBase Not Connected'));//Chek the Connection exists or Not
+
+
+//When The Database Successfully Connected
+data_base.once('on',function(){
+    console.log('DATABASE SUCCESSFULLY CONNECTED');
+});
+
